0d72b37ee9a99c3fbec37e5551d8e7359ce7ff1a
acidbutter96 <marknotas@outlook.com>
2021-04-06 20:57:39 -0300

    build: add middleware to specific routes

--- src/routes.js
+++ src/routes.js
@@ -5,26 +5,28 @@ import MonthlySaleController from './app/controllers/MonthlySaleController';
 import VendorController from './app/controllers/VendorController';
 import SessionController from './app/controllers/SessionController';
 
+import AuthMiddleware from './app/middlewares/auth';
+
 const routes = new Router();
 
 routes.get('/v1/session', SessionController.create);
 
-routes.get('/v1/vendor/:id', VendorController.show);
-routes.get('/v1/vendors', VendorController.index);
+routes.get('/v1/vendor/:id', AuthMiddleware, VendorController.show);
+routes.get('/v1/vendors', AuthMiddleware, VendorController.index);
 routes.post('/v1/vendors', VendorController.store);
-routes.put('/v1/vendors', VendorController.update);
-routes.delete('/v1/vendor/', VendorController.destroy);
+routes.put('/v1/vendors', AuthMiddleware, VendorController.update);
+routes.delete('/v1/vendor/', AuthMiddleware, VendorController.destroy);
 
-routes.get('/v1/goal/:id', MonthlyGoalController.show);
-routes.get('/v1/goals', MonthlyGoalController.index);
-routes.post('/v1/goals', MonthlyGoalController.store);
-routes.put('/v1/goals', MonthlyGoalController.update);
-routes.delete('/v1/goal/', MonthlyGoalController.destroy);
+routes.get('/v1/goal/:id', AuthMiddleware, MonthlyGoalController.show);
+routes.get('/v1/goals', AuthMiddleware, MonthlyGoalController.index);
+routes.post('/v1/goals', AuthMiddleware, MonthlyGoalController.store);
+routes.put('/v1/goals', AuthMiddleware, MonthlyGoalController.update);
+routes.delete('/v1/goal/', AuthMiddleware, MonthlyGoalController.destroy);
 
-routes.get('/v1/sale/:id', MonthlySaleController.show);
-routes.get('/v1/sales', MonthlySaleController.index);
-routes.post('/v1/sales', MonthlySaleController.store);
-routes.put('/v1/sales', MonthlySaleController.update);
-routes.delete('/v1/sale/', MonthlySaleController.destroy);
+routes.get('/v1/sale/:id', AuthMiddleware, MonthlySaleController.show);
+routes.get('/v1/sales', AuthMiddleware, MonthlySaleController.index);
+routes.post('/v1/sales', AuthMiddleware, MonthlySaleController.store);
+routes.put('/v1/sales', AuthMiddleware, MonthlySaleController.update);
+routes.delete('/v1/sale/', AuthMiddleware, MonthlySaleController.destroy);
 
 export default routes;
